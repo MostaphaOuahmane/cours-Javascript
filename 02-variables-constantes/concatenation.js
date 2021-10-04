00e7ce8b850f3f62f0e85234d8850f942108a113
MostaphaOuahmane <87656171+MostaphaOuahmane@users.noreply.github.com>
2021-10-04 16:54:01 +0200

    comit

---
+++ 02-variables-constantes/concatenation.js
@@ -0,0 +1,29 @@
+// parenthèses parenthesis ()
+// crochets brakets []
+// accolades {}
+
+
+// script pour la pagr concatination 
+// alert('coucou!') ;
+// on va faire une variable dans laquelle on met notre age avec un calcul
+
+let monAge =18+2;
+console.log(monAge);
+
+let phrase = 'Bonjour j\'ai';
+
+console.log(phrase);
+console.log(monAge,phrase);
+
+// alert ( phrase +' '+ monAge + ' ans. ' );
+let x = 501;
+let y = 99;
+console.log(x+y);
+// mini exo écrire dans alerte "x contient 501", y contient 99; leur somme est égale à 600
+// console.log ('x contient' + x + 'y contient' + 'leur somme égale à ' + (x+y));
+// alert (' x contient ' + x +  ' y contient ' + ' leur somme égale à  '  +  (x+y);
+// alert(`x contient ${x}`);
+// alert(`y contient ${y} `);
+
+alert(`x contient ${x}  y contient ${y} leur somme égale à ${x+y} `);
+
